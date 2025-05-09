04286ce4e5a6f5ae028898147f6f4d56da7ec7e8
Dmitriy Kozenko <8863314@gmail.com>
2025-05-09 16:36:59 +0300

    remove bin files from git

---
+++ create_er_diagram/generate.py
@@ -0,0 +1,2 @@
+from eralchemy import render_er
+render_er('postgres://postgres:cgsgforever@localhost/bank', 'erd.png')
--- create_er_diagram/make_er.sh
+++ create_er_diagram/make_er.sh
@@ -1,1 +1,2 @@
-schemaspy -t pgsql -db pet-db -host localhost -u postgres -p cgsgforever -o ./output_dir -dp postgresql-42.7.3.jar
+# schemaspy -t pgsql -db pet-db -host localhost -u postgres -p cgsgforever -o ./output_dir -dp postgresql-42.7.3.jar
+schemaspy -t pgsql -db bank -host localhost -u postgres -p cgsgforever -o ./output_dir -dp postgresql-42.7.3.jar
